7b8d32e6dfdd8bbe81d27a99bbaaa3fa62504c58
bschloz <bschloz@jobtechnologies.com>
2019-05-09 12:01:15 -0500

    Remove duplicate constants (from project merge).

--- SqlInstaller/Constants.cs
+++ SqlInstaller/Constants.cs
@@ -33,66 +33,6 @@ namespace JobTech.SqlInstaller
         public const string SQLInstallerXml = "SQLInstaller.xml";
 
         /// <summary>
-        /// The default database name.
-        /// </summary>
-        public const string SingleWhitespace = " ";
-
-        /// <summary>
-        /// The OptIn application setting.
-        /// </summary>
-        public const string OptIn = "OptIn";
-
-        public const string RTM = "RTM";
-        public const string SqlServer = "sqlserver";
-        public const string MySql = "mysql";
-        public const string SQLite = "sqlite";
-        public const string Oracle = "oracle";
-        public const string PostGres = "postgres";
-        public const string Firebird = "firebird";
-        public const string Teradata = "teradata";
-        public const string Azure = "azure";
-        public const string CreateDatabase = "CreateDatabase";
-        public const string DropDatabase = "DropDatabase";
-        public const string DataSource = "Data Source";
-
-        public const string DefaultConfigFile = @".\SQLInstaller.xml";
-        public const string DefaultProvider = "SqlServer";
-        public const string DefaultConnString = "Data Source=localhost;Integrated Security=SSPI;";
-        public const string CipherFile = "SQLInstaller.aes";
-        public const string ProviderFactory = "ProviderFactory";
-
-        public const char Pipe = '|';
-        public const char SplitChar = ';';
-        public const char NewLine = '\n';
-        public const char Space = ' ';
-        public const char BackSlash = '\\';
-        public const char ForwardSlash = '/';
-        public const string CrLf = "\r\n";
-        public const string Tab = "\t";
-        public const string CurrentDir = @".\";
-        public const string XmlExt = ".xml";
-        public const string OpenBracket = "[";
-        public const string Dot = ".";
-        public const string Asterisk = "*";
-        public const string CloseBracketHyphen = "] - ";
-        public const string ResourcesExt = ".resources";
-
-        public const string DefaultInstallPath = "Install";
-        public const string DefaultUpgradePath = "Upgrade";
-        public const string DefaultScriptExtension = ".sql";
-
-        public const string Begin = "BEGIN";
-        public const string OracleAlterSession = "ALTER SESSION SET CURRENT_SCHEMA=";
-
-        public const string DB2AlterSession = "SET SCHEMA ";
-        public const string TeradataAlterSession = "DATABASE ";
-
-        public const string FbScript = "FirebirdSql.Data.Isql.FbScript";
-        public const string FbBatchExecution = "FirebirdSql.Data.Isql.FbBatchExecution";
-        public const string Parse = "Parse";
-        public const string Execute = "Execute";
-
-        /// <summary>
         /// Prevents a default instance of the Constants class from being created.
         /// </summary>
         private Constants()
